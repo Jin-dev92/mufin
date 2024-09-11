e31e9de74fdb73fc43fcb6c275cbc9a0532c97bf
Jin-devST <kpasd002@gmail.com>
2024-09-11 11:14:47 +0900

    업로드 라이브러리 추가 및 글로벌 환경 변수 설정

--- apps/mufin-nestjs/src/app.module.ts
+++ apps/mufin-nestjs/src/app.module.ts
@@ -1,9 +1,18 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import * as process from 'node:process';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: ['development', 'production'].includes(process.env.NODE_ENV)
+        ? `.${process.env.NODE_ENV}.env`
+        : '.env',
+      isGlobal: true,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- apps/mufin-nestjs/src/main.ts
+++ apps/mufin-nestjs/src/main.ts
@@ -3,6 +3,8 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  /* 글로벌 세팅 */
+  app.useGlobalInterceptors();
   await app.listen(3000);
 }
 bootstrap();
