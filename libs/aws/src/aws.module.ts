a662b33dab54fc5ec34eb7a7fd50148ed07ce6a2
Jin-devST <kpasd002@gmail.com>
2024-09-20 18:54:57 +0900

    feat. aws 모듈 추가

---
+++ libs/aws/src/aws.module.ts
@@ -0,0 +1,17 @@
+import { Module } from '@nestjs/common';
+import { AwsService } from './aws.service';
+import { MulterModule } from '@nestjs/platform-express';
+import { ConfigService } from '@nestjs/config';
+import { multerOptionsFactory } from '@libs/aws/factory';
+
+@Module({
+  imports: [
+    MulterModule.registerAsync({
+      useFactory: multerOptionsFactory,
+      inject: [ConfigService],
+    }),
+  ],
+  providers: [AwsService],
+  exports: [AwsService],
+})
+export class AwsModule {}
