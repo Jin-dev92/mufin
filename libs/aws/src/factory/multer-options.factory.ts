0ef8781ec52073e2f4b4eefacccd8eb9b20000c6
Jin-devST <kpasd002@gmail.com>
2024-09-21 12:37:57 +0900

    feat. 이미지, 오디오, 비디오 업로드 기능 구현

--- libs/aws/src/factory/multer-options.factory.ts
+++ libs/aws/src/factory/multer-options.factory.ts
@@ -27,7 +27,7 @@ export const multerOptionsFactory = (
         const ext = extname(file.originalname); // 확장자
         const baseName = basename(file.originalname, ext); // 확장자 제외
         const fileName = `${fileType}s/${Date.now()}_${baseName}${ext}`;
-        console.log('@@@@@@', fileName);
+        console.log(_req);
         callback(null, fileName);
       },
     }),
