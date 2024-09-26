7ecad30de36427343b576be5cb65caa420249e0e
Jin-devST <kpasd002@gmail.com>
2024-09-26 11:30:29 +0900

    test. CI/CD 테스트

--- apps/mufin-nestjs/src/main.ts
+++ apps/mufin-nestjs/src/main.ts
@@ -1,11 +1,10 @@
 import { NestFactory } from '@nestjs/core';
-import * as process from 'node:process';
 import { AppModule } from './modules';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   /* 글로벌 세팅 */
   // app.useGlobalInterceptors();
-  await app.listen(process.env.PORT || 5000);
+  await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
